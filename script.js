b5efd53dfa87e726fe947095e82ab4c59747583e
PeterBookmyer <101828030+PeterBookmyer@users.noreply.github.com>
2022-05-08 21:45:52 -0600

    Code updated with javascript to drive functionality

--- script.js
+++ script.js
@@ -1,5 +1,73 @@
 // Assignment code here
 
+var lengthMin = 8;
+var lengthMax = 128;
+var specialCharacters = "!@#$%^&*()_+={}?";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "1234567890"
+
+function generatePassword() {
+    var pwordLength = prompt("Please enter desired password length between 8-128 characters")
+    console.log(pwordLength)
+    while (pwordLength < lengthMin || pwordLength > lengthMax){
+        pwordLength = prompt("Please enter a number between 8-128 characters")
+        console.log(pwordLength)}
+    var pwordCharacters = confirm("Would you like to include special characters?")
+    console.log (pwordCharacters)
+    var useUpperCase = confirm("Woudld you like to include Upper Case?")
+    console.log (useUpperCase)
+    var useLowerCase = confirm("Would you like to include lower case?")
+    console.log (useLowerCase)
+    var useNumbers = confirm("Would you like to include numbers?")
+    console.log (useNumbers)
+    
+    var passArray = ""
+    
+    if (pwordCharacters && useUpperCase && useLowerCase && useNumbers) {
+        var passArray = lowerCase.concat(specialCharacters, upperCase, numbers)
+        console.log (passArray)
+     }
+    else if (useUpperCase && useLowerCase && useNumbers) {
+        var passArray = upperCase.concat(lowerCase, numbers)
+        console.log (passArray)
+    }
+    else if (useLowerCase && useNumbers) {
+        var passArray = lowerCase.concat(numbers)
+        console.log (passArray)
+    }
+     else if (pwordCharacters && useUpperCase && useNumbers) {
+        var passArray = specialCharacters.concat(upperCase, numbers)
+        console.log (passArray)
+    }
+     else if (pwordCharacters && useNumbers) {
+        var passArray = specialCharacters.concat(numbers)
+        console.log (passArray)
+    }
+    else if (useUpperCase && useNumbers) {
+        var passArray = upperCase.concat(numbers)
+        console.log (passArray)
+    }
+    else if (useUpperCase && useLowerCase) {
+        var passArray = upperCase.concat(lowerCase)
+        console.log (passArray)
+    }
+    else {
+        var passArray = lowerCase
+        console.log (passArray)
+    }
+
+    let password = "";
+    var passAmount = passArray.length
+    pwordLength = parseInt(pwordLength)
+    for (var i=0; i<pwordLength; i++) {
+    password += passArray.charAt(Math.floor(Math.random * passAmount))
+    }
+    
+    console.log (password);
+    return password;
+
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -9,7 +77,6 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
 }
 
